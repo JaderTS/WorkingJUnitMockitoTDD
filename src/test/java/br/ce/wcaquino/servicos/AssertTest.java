b3446f75b4d52f4fd9ee24b18027eb543ad89e7a
jaderts <jader-fernandes@live.com>
2020-10-01 21:15:45 -0300

    working with assert

--- src/test/java/br/ce/wcaquino/servicos/AssertTest.java
+++ src/test/java/br/ce/wcaquino/servicos/AssertTest.java
@@ -10,6 +10,7 @@ public class AssertTest {
         Assert.assertTrue(true);
         Assert.assertFalse(false);
 
+        //Assert.assertEquals("Erro na comparação",1,2);
         Assert.assertEquals(1,1);
         Assert.assertEquals(0.51,0.59265656,0.09);
         Assert.assertEquals(Math.PI, 3.14, 0.01);
@@ -30,6 +31,9 @@ public class AssertTest {
         Assert.assertEquals(u1,u2);
 
         Assert.assertSame(u2, u2);
+        Assert.assertNotSame(u1,u3);;
 
+        Assert.assertNull(u3);
+        Assert.assertNotNull(u1);
     }
 }
